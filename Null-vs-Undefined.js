0364a77a128e0e548fd70d110484d3769b5372fa
smzahurul <zahurul.it@gmail.com>
2022-05-10 22:24:28 +0600

    NaN vs undefinedl

---
+++ Null-vs-Undefined.js
@@ -0,0 +1,23 @@
+// Null Vs Undefined
+
+// Undefined Value below;
+let webView;
+console.log(webView);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2) {
+    console.log(num1.num2);
+
+}
+const result = add(12);
+console.log(result);
+
+const premik = { name: 'Rohim', phone: 1234565 };
+console.log(premik.gfName);
+
+let fun = undefined;
+console.log(fun)
