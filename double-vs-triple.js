8da78ee32fc367fe89b33b929fd46feecaa7e63f
smzahurul <zahurul.it@gmail.com>
2022-05-10 22:40:37 +0600

    double vs triple

---
+++ double-vs-triple.js
@@ -0,0 +1,21 @@
+// double-vs-triple
+// start 0= false;
+// start 1 = true;
+
+// double == value check;
+const first = 4;
+const second = 7;
+if (first == second) {
+    console.log('condition is true');
+} else {
+    console.log('condition is false');
+}
+
+// triple === value and type check;
+const first1 = 4;
+const second1 = 7;
+if (first === second) {
+    console.log('condition is true');
+} else {
+    console.log('condition is false');
+}
