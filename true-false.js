2fa90eecced8b88adfa0e346b9da2bfb01dc44f4
smzahurul <zahurul.it@gmail.com>
2022-05-10 22:00:40 +0600

    true vs false value

---
+++ true-false.js
@@ -0,0 +1,29 @@
+// const age = 7;
+// const name = " ";
+
+// falsy Value below;
+// 0;
+// "";
+// undefined;
+// null;
+// NaN;
+
+// truthy Value below;
+// '0';
+// " ";
+// [];
+// {};
+const name = " ";
+let name1;
+
+if (name) {
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+
+}
+if (name1) {
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
